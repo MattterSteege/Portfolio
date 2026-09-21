4d81e0beb40a3869ee55bba2ac009a1082c8a0d2
MJTSgamer <matttersteege@gmail.com>
2026-09-21 18:04:13 +0200

    started on icon placement

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var timeAlive = document.getElementById("time-alive");
+var backstory = document.getElementById("backstory");
+var backstoryBox = document.getElementById("backstory-box");
+var backstoryStoryBox = document.getElementById("backstory-story-box");
 
 var dateOfBirth = new Date("2007-05-24T00:00:00"); // Replace with your date of birth
+var currentBackstory = 0;
 
 //=======================================
 timeAlive.innerHTML = calculateTimeAlive(dateOfBirth);
@@ -11,4 +15,47 @@ function calculateTimeAlive(dob) {
     var timeDiff = now - dob; // Difference in milliseconds
     var daysAlive = Math.floor(timeDiff / (1000 * 60 * 60 * 24)); // Convert to days
     return "DAY " + daysAlive;
+}
+
+backstory.addEventListener("click", function() {
+    backstoryBox.style.opacity = "1";
+});
+
+document.addEventListener("click", function(event) {
+    if (currentBackstory >= 0 && currentBackstory < 3 && event.target !== backstory && backstoryBox.style.opacity === "1") {
+        nextStoryPage();
+    }
+    else {
+        backstoryBox.style.opacity = "0";
+        currentBackstory = 0;
+    }
+});
+
+
+
+function nextStoryPage(){
+    var texts = [
+        "Hi, I'm Matt!",
+        "I was born on May 24, 2007.",
+        "I love coding and creating new things."
+    ]
+
+    movieWrite(backstoryStoryBox, texts[currentBackstory], 50);
+    currentBackstory++;
+}
+
+let interval;
+
+function movieWrite(element, text, speed) {
+    clearInterval(interval);
+    element.innerHTML = "​"; // Clear the element's content
+    let i = 0;
+     interval = setInterval(function() {
+        if (i < text.length) {
+            element.innerHTML += text.charAt(i);
+            i++;
+        } else {
+            clearInterval(interval);
+        }
+    }, speed);
 }
